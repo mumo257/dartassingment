e5839147a3d8614f7a4b13fa556d778031cd7f1a
mumo257 <festusmumo06@gmail.com>
2023-09-07 12:06:35 +0300

    first commit

---
+++ math.dart
@@ -0,0 +1,14 @@
+import "dart:math";
+
+void main() {
+  int num1 = 50;
+  int num2 = 23;
+  var addition = num1 + num2;
+  print(addition);
+  var multiply = num1 * num2;
+  print(multiply);
+  var subtraction = num1 - num2;
+  print(subtraction);
+  print(sqrt(num1));
+  print(sqrt(num2));
+}
